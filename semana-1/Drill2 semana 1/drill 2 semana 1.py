32daf955e8150cfe67615309e841a6574deebc57
unknown <noeescorp@gmail.com>
2026-09-09 01:53:22 -0600

    actualizcon desde pc

---
+++ semana-1/Drill2 semana 1/drill 2 semana 1.py
@@ -0,0 +1,13 @@
+# DRILL 1: EL REGISTRO DE BANDAS DE MATEO
+
+# Pedir el nombre de la banda
+nombre_banda = input("Ingresa el nombre de la banda: ")
+
+# Pedir el escenario asignado
+escenario = input("Ingresa el escenario asignado: ")
+
+# Pedir el horario de la prueba de sonido
+horario = input("Ingresa el horario de la prueba de sonido: ")
+
+# Mostrar el resumen del registro
+print(f"Registro confirmado: {nombre_banda} — Escenario {escenario} — {horario}")
