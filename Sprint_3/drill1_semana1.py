810b5f25656cb44d98a1ce0066a0b6548247fe9b
unknown <noeescorp@gmail.com>
2026-09-25 00:45:24 -0600

    avance drills 1 y 2 sprint3

---
+++ Sprint_3/drill1_semana1.py
@@ -0,0 +1,9 @@
+# DRILL 1: EL CONTADOR REGRESIVO DE SOFÍA
+
+dias_restantes = 7
+
+while dias_restantes > 0:
+    print(f"Faltan {dias_restantes} días para SonidoLibre")
+    dias_restantes -= 1
+
+print("¡Hoy arranca el festival!")
---
+++ Sprint_3/drill2_semana1.py
@@ -0,0 +1,8 @@
+# DRILL 2: LA VALIDACIÓN OBSTINADA DE VALENTINA
+
+palabra = input("Palabra clave del día: ")
+
+while palabra != "area-tecnica":
+    palabra = input("Incorrecto. Intenta de nuevo: ")
+
+print("Acceso a zona técnica concedido")
